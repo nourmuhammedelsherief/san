35a5081d22d795538899e7bf4cec30c0129bcb84
Nour Muhammed <nourmuhammed20121994@gmail.com>
2023-10-17 16:25:58 +0300

    get student subjects form his ratesdsss

--- app/Http/Controllers/Api/StudentController/AuthStudentController.php
+++ app/Http/Controllers/Api/StudentController/AuthStudentController.php
@@ -73,7 +73,10 @@ class AuthStudentController extends Controller
     public function my_subjects(Request $request)
     {
         $subjects = StudentRate::whereStudentId($request->user()->id)
-            ->select('*')
+            ->select('id' ,'rate_id',
+                'student_id',
+                'points',
+                'subject_id')
             ->groupBy('subject_id')
             ->get();
         return ApiController::respondWithSuccess(SubjectResource::collection($subjects));
