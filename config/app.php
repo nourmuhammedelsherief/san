449d13b97618ac00d951d1bf0778aed21804123b
Nour Muhammed <nourmuhammed20121994@gmail.com>
2023-11-26 12:41:58 +0200

    responsive

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Sanaidi'),
 
     /*
     |--------------------------------------------------------------------------
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://plus.snaydii.com'),
 
     'asset_url' => env('ASSET_URL'),
 
